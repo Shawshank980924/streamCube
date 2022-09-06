9ea15b2f8f4bdd56568d2e7a12c82d54b205f06d
Shawshank980924 <Akatsuki980924@163.com>
2022-09-06 11:33:10 +0800

    xxx_received

--- src/main/java/cn/com/bsfit/assets/dd/dim/Account.java
+++ src/main/java/cn/com/bsfit/assets/dd/dim/Account.java
@@ -27,6 +27,8 @@ public class Account {
 
     private Long firstTxnTime;
 
+    private Long lastTxnTime;
+
     private Long out_degree;
 
     private Long in_degree;
@@ -52,6 +54,15 @@ public class Account {
     private Double avg_value_sent_to_contracts;
 
     private Double total_ether_sent_for_contracts;
+
+    private Double xxx_sent_to_contracts;
+
+    private Double xxx_sent;
+
+    private Double xxx_received;
+
+
+
 
 
 
@@ -102,6 +113,14 @@ public class Account {
         this.firstTxnTime = firstTxnTime;
     }
 
+    @Field("账户最后一次发送交易的时间")
+    public Long getLastTxnTime() {
+        return lastTxnTime;
+    }
+    public void setlastTxnTime(Long lastTxnTime) {
+        this.lastTxnTime= lastTxnTime;
+    }
+
     @Field("节点出度")
     public Long getOut_degree() {
         return out_degree;
@@ -126,7 +145,7 @@ public class Account {
         this.avg_time_between_sent = avg_time_between_sent;
     }
 
-    @Field("账户最近三个月的交易流入")
+    @Field("账户的交易流入邻居")
     public Set<String> getIn_address() {
         return in_address;
     }
@@ -134,7 +153,7 @@ public class Account {
         this.in_address = in_address;
     }
 
-    @Field("账户最近三个月的交易流出")
+    @Field("账户的交易流出邻居")
     public Set<String> getOut_address() {
         return out_address;
     }
@@ -190,7 +209,7 @@ public class Account {
         this.total_ether_sent_for_accounts = total_ether_sent_for_accounts;
     }
 
-    @Field("向合约发送的最小金额 ")
+    @Field("向合约发送的最小金额")
     public Double getMin_value_sent_to_contracts() {
         return min_value_sent_to_contracts;
     }
@@ -198,7 +217,7 @@ public class Account {
         this.min_value_sent_to_contracts= min_value_sent_to_contracts;
     }
 
-    @Field("向合约发送的最大金额 ")
+    @Field("向合约发送的最大金额")
     public Double getMax_value_sent_to_contracts() {
         return max_value_sent_to_contracts;
     }
@@ -206,7 +225,7 @@ public class Account {
         this.max_value_sent_to_contracts= max_value_sent_to_contracts;
     }
     
-    @Field("向合约发送的平均金额 ")
+    @Field("向合约发送的平均金额")
     public Double getAvg_value_sent_to_contracts() {
         return avg_value_sent_to_contracts;
     }
@@ -214,13 +233,40 @@ public class Account {
         this.avg_value_sent_to_contracts= avg_value_sent_to_contracts;
     }
 
-    @Field("向合约发送的总金额 ")
+    @Field("向合约发送的总金额")
     public Double getTotal_ether_sent_for_contracts() {
         return total_ether_sent_for_contracts;
     }
     public void setTotal_ether_sent_for_contracts(Double total_ether_sent_for_contracts) {
         this.total_ether_sent_for_contracts= total_ether_sent_for_contracts;
     }
+
+    @Field("账户向合约发送的xxx代币金额")
+    public Double getXxx_sent_to_contracts() {
+        return xxx_sent_to_contracts;
+    }
+    public void setXxx_sent_to_contracts(Double xxx_sent_to_contracts) {
+        this.xxx_sent_to_contracts= xxx_sent_to_contracts;
+    }
+    
+    @Field("账户发送的xxx代币金额")
+    public Double getXxx_sent() {
+        return xxx_sent;
+    }
+    public void setXxx_sent(Double xxx_sent) {
+        this.xxx_sent= xxx_sent;
+    }
+
+    @Field("账户接收的xxx代币金额")
+    public Double getXxx_received() {
+        return xxx_received;
+    }
+    public void setXxx_received(Double xxx_received) {
+        this.xxx_received= xxx_received;
+    }
+
+
+
 
 
 
